0a5dcfc3c905dfbe93c0fbb19f1cc6fbb33b3c40
Cozmo <mohamed@lenovo.local>
2021-11-17 16:00:53 +0200

    ss

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -28,17 +28,17 @@ const Header = () => (
     </Div1>
     <Div2>
       <li>
-        <Link href='#projects'>
+        <Link href='/My-Portofolio/#projects'>
           <NavLink>Projects</NavLink>
         </Link>
       </li>
       <li>
-        <Link href='#tech'>
+        <Link href='/My-Portofolio/#tech'>
           <NavLink>Technologies</NavLink>
         </Link>
       </li>
       <li>
-        <Link href='#about'>
+        <Link href='/My-Portofolio/#about'>
           <NavLink>About</NavLink>
         </Link>
       </li>
