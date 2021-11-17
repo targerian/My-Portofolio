d13d363b819b106399546dd9f764a4914843702c
Cozmo <mohamed@lenovo.local>
2021-11-17 15:40:15 +0200

    fixing links 2nd try

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -20,38 +20,38 @@ import {
 const Header = () => (
   <Container>
     <Div1>
-      <Link href="/">
+      <Link href='/'>
         <a style={{ display: "flex", alignItems: "center", color: "white" }}>
-          <DiCssdeck size="3rem" /> <span> M-Ali</span>
+          <DiCssdeck size='3rem' /> <span> M-Ali</span>
         </a>
       </Link>
     </Div1>
     <Div2>
       <li>
-        <Link href="My-Portofolio/#projects">
+        <Link href='/My-Portofolio/#projects'>
           <NavLink>Projects</NavLink>
         </Link>
       </li>
       <li>
-        <Link href="My-Portofolio/#tech">
+        <Link href='/My-Portofolio/#tech'>
           <NavLink>Technologies</NavLink>
         </Link>
       </li>
       <li>
-        <Link href="My-Portofolio/#about">
+        <Link href='/My-Portofolio/#about'>
           <NavLink>About</NavLink>
         </Link>
       </li>
     </Div2>
     <Div3>
-      <SocialIcons href="https://github.com/targerian">
-        <AiFillGithub size="3rem" />
+      <SocialIcons href='https://github.com/targerian'>
+        <AiFillGithub size='3rem' />
       </SocialIcons>
-      <SocialIcons href="https://www.facebook.com/Goodguycozmo/">
-        <AiFillFacebook size="3rem" />
+      <SocialIcons href='https://www.facebook.com/Goodguycozmo/'>
+        <AiFillFacebook size='3rem' />
       </SocialIcons>
-      <SocialIcons href="https://www.instagram.com/muhammed.ali/">
-        <AiFillInstagram size="3rem" />
+      <SocialIcons href='https://www.instagram.com/muhammed.ali/'>
+        <AiFillInstagram size='3rem' />
       </SocialIcons>
     </Div3>
   </Container>
